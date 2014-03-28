971e348da89c12c9e33f10b15ed580879a290fb6
Andy Phan <phandy@gmail.com>
2014-03-28 18:50:02 -0400

    Added pagination to the trees page.

--- app/controllers/admin/trees_controller.rb
+++ app/controllers/admin/trees_controller.rb
@@ -4,6 +4,14 @@ class Admin::TreesController < ApplicationController
 
   def index
 	verify_is_admin()
+	
+    @trees = Tree.order('id').page(params[:page]).per(50)
+ 
+    respond_to do |format|
+      format.html  # index.html.erb
+      format.json  { render :json => @trees }
+    end
   end
-
+  def import
+  end
 end
